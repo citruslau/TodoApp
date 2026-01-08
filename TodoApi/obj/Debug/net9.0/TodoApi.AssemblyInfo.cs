93edde3e338a951a0d163ba4ac585b4b68516214
citruslau <gerald.latagan@gmail.com>
2026-01-09 07:46:36 +0800

    chore: fix gitignore and remove build artifacts from tracking

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b610935c448d35ff1d76f72751e0d0724c2bb89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65397a24bb1ca52b2799bf2d651d7948fd10404c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
